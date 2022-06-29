e722d8ec54bf16da660d73a78b9c0b21cdc7bf0c
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-06-29 13:59:53 +0200

    Add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// Diseña
+const designTitle = document.querySelector('.js_design');
+const divDesign = document.querySelector('.js_divDesign');
+const iconDesign = document.querySelector('.js_iconDesign');
+
+designTitle.addEventListener('click', () => {
+  divDesign.classList.toggle('collapsed');
+  iconDesign.classList.toggle('rotate');
+});
+
+// Rellena
+
+const fillTitle = document.querySelector('.js_fillTitle');
+const divFIll = document.querySelector('.js_divFill');
+const iconFill = document.querySelector('.js_iconFill');
+
+fillTitle.addEventListener('click', () => {
+  divFIll.classList.toggle('collapsed');
+  iconFill.classList.toggle('rotate');
+  divDesign.classList.add('collapsed');
+});
+
+// Comparte
+
+const shareTitle = document.querySelector('.js_shareTitle');
+const divShare = document.querySelector('.js_divShare');
+const iconShare = document.querySelector('.js_iconShare');
+const buttonShare = document.querySelector('.js_shareButton');
+
+shareTitle.addEventListener('click', () => {
+  iconShare.classList.toggle('rotate');
+  divDesign.classList.add('collapsed');
+  buttonShare.classList.remove('collapsed');
+});
+
+buttonShare.addEventListener('click', () =>{
+  divShare.classList.remove('collapsed');
+});
+
+
+// function toggleCollapsed(parClass) {
+//   parClass.classList.toggle('collapsed');
+// }
