9590570d47862f9c158f6cf67e0265d1d465b96e
modehans <monicaglezsanch@gmail.com>
2022-07-04 13:17:22 +0200

    Add fuction add image preview and change in css styles

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -12,6 +12,9 @@ const previewPhone = document.querySelector ('.js_previewPhone');
 const previewEmail = document.querySelector ('.js_previewEmail');
 const previewLinkedine = document.querySelector ('.js_previewLinkedin');
 const previewGithub = document.querySelector ('.js_previewGithub');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
 
 const dataInput = {
   palette: 1,
@@ -29,3 +32,42 @@ const handleInput = () =>{
   previewName.innerHTML = dataInput.name;
 };
 inputName.addEventListener('keyup', handleInput);
+
+
+//Capturar imagen y visualizar
+const fr = new FileReader();
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que 
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e 
+ */
+function getImage(e){
+  const myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado. Ese resultado de procesar el fichero que hemos cargado
+   * podemos pasarlo como background a la imagen de perfil y a la vista previa
+   * de nuestro componente.
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+
+
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+fileField.addEventListener('change', getImage);
