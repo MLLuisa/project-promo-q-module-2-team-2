88dbd664f1894884feb1e50eaca112ef97eaa2c5
silvia <silvia.sg@hotmail.es>
2022-07-08 13:21:20 +0200

    btn reset

---
+++ src/js/00-const.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+// Diseña
+const designTitle = document.querySelector(".js_designTitle");
+const divDesign = document.querySelector(".js_divDesign");
+const iconDesign = document.querySelector(".js_iconDesign");
+
+// designTitle.addEventListener('click', () => {
+//   divDesign.classList.toggle('collapsed');
+//   iconDesign.classList.toggle('rotate');
+// });
+
+// Rellena
+
+const fillTitle = document.querySelector(".js_fillTitle");
+const divFill = document.querySelector(".js_divFill");
+const iconFill = document.querySelector(".js_iconFill");
+
+// fillTitle.addEventListener('click', () => {
+//   divFIll.classList.toggle('collapsed');
+//   iconFill.classList.toggle('rotate');
+//   divDesign.classList.add('collapsed');
+// });
+
+// Comparte
+
+const shareTitle = document.querySelector(".js_shareTitle");
+const divShare = document.querySelector(".js_divShare");
+const iconShare = document.querySelector(".js_iconShare");
+const buttonShare = document.querySelector(".js_shareButton");
+const cardShare = document.querySelector(".js_cardShare");
+
+
+const inputName = document.querySelector ('.js_inputName');
+const inputJob = document.querySelector ('.js_inputJob');
+const inputEmail = document.querySelector ('.js_inputEmail');
+const inputPhone = document.querySelector ('.js_inputPhone');
+const inputLinkedin = document.querySelector ('.js_inputLinkedin');
+const inputGithub = document.querySelector ('.js_inputGithub');
+
+const previewName = document.querySelector ('.js_previewName');
+const previewJob = document.querySelector ('.js_previewJob');
+const previewPhone = document.querySelector ('.js_previewPhone');
+const previewEmail = document.querySelector ('.js_previewEmail');
+const previewLinkedin = document.querySelector ('.js_previewLinkedin');
+const previewGithub = document.querySelector ('.js_previewGithub');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
+const allInputs = document.querySelector('.js_allInputs');
+
+const btnReset = document.querySelector('.js_btnReset');
