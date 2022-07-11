48bac26b5623f7b5a5f52500587fc94d2ffe9157
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-07-11 13:17:19 +0200

    add palettes

--- src/js/04-palettes.js
+++ src/js/04-palettes.js
@@ -1,31 +1,19 @@
-const iconsColor = document.querySelectorAll(".js_icons");
-const rectangleColor = document.querySelector(".js_preview__rectangle");
-const circleColor = document.querySelectorAll(".js_preview_icon");
+"use strict";
 
-//Traer de DesingHTML los value del input con el target
+const allInputsRadio = document.querySelectorAll(".js_inputRadio");
+const previewBox = document.querySelector(".js_previewBox");
 
-//RESTRUCTURAR
-
-function changeColor1() {
-  previewName.style.color = "#114e4e";
-  iconsColor.style.color = "#114e4e";
-  rectangleColor.style.backgroundColor = "#438792";
-  circleColor.style.borderColor = "#a2deaf";
-  //Hacer for para cada uno
+function paintPalette(palette) {
+  previewBox.classList.remove("palette-1", "palette-2", "palette-3");
+  previewBox.classList.add(`palette-${palette}`);
 }
 
-function changeColor2() {
-  previewName.style.color = "#420101";
-  iconsColor.style.color = "#420101";
-  rectangleColor.style.backgroundColor = "#bd1010";
-  circleColor.style.borderColor = "#e95626";
+function handleRadio(ev) {
+  const palette = parseInt(ev.currentTarget.value);
+  dataInput.palette = palette;
+  paintPalette(palette);
 }
 
-function changeColor3() {
-  previewName.style.color = "#3e5b65";
-  iconsColor.style.color = "#3e5b65";
-  rectangleColor.style.backgroundColor = "#eab052";
-  circleColor.style.borderColor = "#a0c0cf";
+for (const oneRadio of allInputsRadio) {
+  oneRadio.addEventListener("click", handleRadio);
 }
-console.log(Array.isArray(circleColor));
-console.log(circleColor);
