acc8f020ec54f16132898b8da03df9a097560574
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-07-11 14:07:36 +0200

    Add reset checked

--- src/js/00-const.js
+++ src/js/00-const.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 // Diseña
 const designTitle = document.querySelector(".js_designTitle");
@@ -31,23 +30,23 @@ const iconShare = document.querySelector(".js_iconShare");
 const buttonShare = document.querySelector(".js_shareButton");
 const cardShare = document.querySelector(".js_cardShare");
 
+const inputName = document.querySelector(".js_inputName");
+const inputJob = document.querySelector(".js_inputJob");
+const inputEmail = document.querySelector(".js_inputEmail");
+const inputPhone = document.querySelector(".js_inputPhone");
+const inputLinkedin = document.querySelector(".js_inputLinkedin");
+const inputGithub = document.querySelector(".js_inputGithub");
 
-const inputName = document.querySelector ('.js_inputName');
-const inputJob = document.querySelector ('.js_inputJob');
-const inputEmail = document.querySelector ('.js_inputEmail');
-const inputPhone = document.querySelector ('.js_inputPhone');
-const inputLinkedin = document.querySelector ('.js_inputLinkedin');
-const inputGithub = document.querySelector ('.js_inputGithub');
+const previewName = document.querySelector(".js_previewName");
+const previewJob = document.querySelector(".js_previewJob");
+const previewPhone = document.querySelector(".js_previewPhone");
+const previewEmail = document.querySelector(".js_previewEmail");
+const previewLinkedin = document.querySelector(".js_previewLinkedin");
+const previewGithub = document.querySelector(".js_previewGithub");
+const fileField = document.querySelector(".js__profile-upload-btn");
+const profileImage = document.querySelector(".js__profile-image");
+const profilePreview = document.querySelector(".js__profile-preview");
+const allInputs = document.querySelector(".js_allInputs");
 
-const previewName = document.querySelector ('.js_previewName');
-const previewJob = document.querySelector ('.js_previewJob');
-const previewPhone = document.querySelector ('.js_previewPhone');
-const previewEmail = document.querySelector ('.js_previewEmail');
-const previewLinkedin = document.querySelector ('.js_previewLinkedin');
-const previewGithub = document.querySelector ('.js_previewGithub');
-const fileField = document.querySelector('.js__profile-upload-btn');
-const profileImage = document.querySelector('.js__profile-image');
-const profilePreview = document.querySelector('.js__profile-preview');
-const allInputs = document.querySelector('.js_allInputs');
-
-const btnReset = document.querySelector('.js_btnReset');
+const btnReset = document.querySelector(".js_btnReset");
+const palette1Reset = document.querySelector(".js_radioReset");
--- src/js/02-inputs.js
+++ src/js/02-inputs.js
@@ -1,50 +1,50 @@
-'use strict';
+"use strict";
 
 const dataInput = {
-  palette: 1,
-  name:'',
-  job: '',
-  phone:'',
-  email: '',
-  linkedin: '',
-  github: '',
-  photo: ''
+  palette: "",
+  name: "",
+  job: "",
+  phone: "",
+  email: "",
+  linkedin: "",
+  github: "",
+  photo: "",
 };
 
 function renderPreview() {
-  if (dataInput.name ===''){
-    previewName.innerHTML =`Nombre apellido`;
-  } else{
+  if (dataInput.name === "") {
+    previewName.innerHTML = `Nombre apellido`;
+  } else {
     previewName.innerHTML = dataInput.name;
   }
-  if (dataInput.job===''){
-    previewJob.innerHTML =`Front-end developer`;
-  } else{
+  if (dataInput.job === "") {
+    previewJob.innerHTML = `Front-end developer`;
+  } else {
     previewJob.innerHTML = dataInput.job;
   }
-  if (dataInput.phone ===''){
-    previewPhone.href =``;
-  } else{
+  if (dataInput.phone === "") {
+    previewPhone.href = ``;
+  } else {
     previewPhone.href = dataInput.phone;
   }
-  if (dataInput.email ===''){
-    previewEmail.href =``;
-  } else{
+  if (dataInput.email === "") {
+    previewEmail.href = ``;
+  } else {
     previewEmail.href = `mailto: ${dataInput.email}`;
   }
-  if (dataInput.linkedin ===''){
-    previewLinkedin.href =``;
-  } else{
+  if (dataInput.linkedin === "") {
+    previewLinkedin.href = ``;
+  } else {
     previewLinkedin.href = dataInput.linkedin;
   }
-  if (dataInput.github ===''){
-    previewGithub.href =``;
-  } else{
+  if (dataInput.github === "") {
+    previewGithub.href = ``;
+  } else {
     previewGithub.href = dataInput.github;
   }
 }
 
-const handleInput = (ev) =>{
+const handleInput = (ev) => {
   const nameInput = ev.target.name;
   const valueInput = ev.target.value;
   dataInput[nameInput] = valueInput;
@@ -52,21 +52,21 @@ const handleInput = (ev) =>{
   console.dir(dataInput);
 };
 
-allInputs.addEventListener('keyup', handleInput);
+allInputs.addEventListener("keyup", handleInput);
 
 //Capturar imagen y visualizar
 const fr = new FileReader();
 /**
  * Recoge el archivo añadido al campo de tipo "file"
- * y lo carga en nuestro objeto FileReader para que 
+ * y lo carga en nuestro objeto FileReader para que
  * lo convierta a algo con lo que podamos trabajar.
  * Añade un listener al FR para que ejecute una función
  * al tener los datos listos
- * @param {evento} e 
+ * @param {evento} e
  */
-function getImage(e){
+function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
+  fr.addEventListener("load", writeImage);
   fr.readAsDataURL(myFile);
 }
 
@@ -85,45 +85,40 @@ function writeImage() {
   dataInput.photo = `url(${fr.result})`; //Guarda url en dataInput
 }
 
-
 /**
  * Añadimos los listeners necesarios:
  * - al botón visible para generar el click automático
  * - al campo oculto para cuando cambie su value
  */
-fileField.addEventListener('change', getImage);
+fileField.addEventListener("change", getImage);
 
-function clearObjectData (){
-  dataInput.palette = 1,
-  dataInput.name='';
-  dataInput.job= '';
-  dataInput.phone='';
-  dataInput.email= '';
-  dataInput.linkedin= '';
-  dataInput.github= '';
-  dataInput.photo= '';
+function clearObjectData() {
+  dataInput.palette = "";
+  dataInput.name = "";
+  dataInput.job = "";
+  dataInput.phone = "";
+  dataInput.email = "";
+  dataInput.linkedin = "";
+  dataInput.github = "";
+  dataInput.photo = "";
 }
 
-
-function clearInput (){
-
-
-inputName.value = '';
-inputJob.value = '';
-inputEmail.value = '';
-inputPhone.value = '';
-inputLinkedin.value = '';
-inputGithub.value = '';
-
+function clearInput() {
+  inputName.value = "";
+  inputJob.value = "";
+  inputEmail.value = "";
+  inputPhone.value = "";
+  inputLinkedin.value = "";
+  inputGithub.value = "";
 }
 
 const handleReset = (ev) => {
   ev.preventDefault();
   clearObjectData();
   renderPreview(dataInput);
-  clearInput ();
+  clearInput();
+  paintPalette(1);
   console.log(dataInput);
 };
 
-
-btnReset.addEventListener('click', handleReset);
+btnReset.addEventListener("click", handleReset);
