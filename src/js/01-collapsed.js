3d353c1897b661dc5643ab33dd2d38961dce3859
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-07-12 18:59:17 +0200

    Fix rotate collapsables

--- src/js/01-collapsed.js
+++ src/js/01-collapsed.js
@@ -1,25 +1,31 @@
-"use strict";
+'use strict';
 
 const handleClick = (ev) => {
   const elementClick = ev.currentTarget;
-  if (elementClick.classList.contains("js_designTitle")) {
-    divDesign.classList.toggle("collapsed");
-    iconDesign.classList.toggle("rotate");
-    divFill.classList.add("collapsed");
-    divShare.classList.add("collapsed");
-  } else if (elementClick.classList.contains("js_fillTitle")) {
-    divFill.classList.toggle("collapsed");
-    iconFill.classList.toggle("rotate");
-    divDesign.classList.add("collapsed");
-    divShare.classList.add("collapsed");
-  } else if (elementClick.classList.contains("js_shareTitle")) {
-    divShare.classList.toggle("collapsed");
-    iconShare.classList.toggle("rotate");
-    divDesign.classList.add("collapsed");
-    divFill.classList.add("collapsed");
+  if (elementClick.classList.contains('js_designTitle')) {
+    divDesign.classList.toggle('collapsed');
+    iconDesign.classList.toggle('rotate');
+    divFill.classList.add('collapsed');
+    iconFill.classList.remove('rotate');
+    divShare.classList.add('collapsed');
+    iconShare.classList.remove('rotate');
+  } else if (elementClick.classList.contains('js_fillTitle')) {
+    divFill.classList.toggle('collapsed');
+    iconFill.classList.toggle('rotate');
+    divDesign.classList.add('collapsed');
+    iconDesign.classList.remove('rotate');
+    divShare.classList.add('collapsed');
+    iconShare.classList.remove('rotate');
+  } else if (elementClick.classList.contains('js_shareTitle')) {
+    divShare.classList.toggle('collapsed');
+    iconShare.classList.toggle('rotate');
+    divDesign.classList.add('collapsed');
+    iconDesign.classList.remove('rotate');
+    divFill.classList.add('collapsed');
+    iconFill.classList.remove('rotate');
   }
 };
 
-designTitle.addEventListener("click", handleClick);
-fillTitle.addEventListener("click", handleClick);
-shareTitle.addEventListener("click", handleClick);
+designTitle.addEventListener('click', handleClick);
+fillTitle.addEventListener('click', handleClick);
+shareTitle.addEventListener('click', handleClick);
