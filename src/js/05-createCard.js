491815c70200fb29364bf44a3c17f028622650a7
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-12 14:17:23 +0200

    Add link share twitter

--- src/js/05-createCard.js
+++ src/js/05-createCard.js
@@ -2,6 +2,7 @@
 
 const sectionUrl = document.querySelector('.js_url');
 const pMsg = document.querySelector('.js_msj');
+const shareTwitter = document.querySelector('.js_shareTwitter');
 
 function createCard() {
   //expresiones regulares ---> validar email, validar contraseñas--> BONUS
@@ -18,6 +19,7 @@ function createCard() {
       if (serverResp.success) {
         sectionUrl.innerHTML = serverResp.cardURL;
         sectionUrl.href = serverResp.cardURL;
+        shareTwitter.href = `https://twitter.com/intent/tweet?text= adalab&url=${serverResp.cardURL}`;
       } else {
         pMsg.innerHTML = 'Todos los campos deben estar llenos.';
       }
@@ -31,4 +33,5 @@ function handleClickButtonShare (ev) {
 }
 
 buttonShare.addEventListener("click", (handleClickButtonShare));
+
 
